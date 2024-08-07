6635c515fd11133da407793bd30942c893786a6a
ratul544388 <ratulhossainbnc@gmail.com>
2024-08-07 18:20:24 +0600

    fix fonts

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,7 +1,42 @@
 import {
+  Chakra_Petch,
   Inter,
+  Kanit,
+  Rowdies,
+  Teko,
+  Gupter,
 } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter" });
 
-export const fonts = `${inter.variable}`;
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["600"],
+  variable: "--kanit",
+});
+
+const chakraPetch = Chakra_Petch({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--chakra_petch",
+});
+
+const rowdies = Rowdies({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--rowdies",
+});
+
+const teko = Teko({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--teko",
+});
+
+const gupter = Gupter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--gupter",
+});
+
+export const fonts = `${inter.variable} ${kanit.variable} ${chakraPetch.variable} ${rowdies.variable} ${teko.variable} ${gupter.variable}`;
